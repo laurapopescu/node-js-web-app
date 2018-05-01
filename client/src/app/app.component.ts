4cc69e4843434d4df26883e71037c3e86c9c68f6
Laura Popescu <laurapopescu@Lauras-MacBook-Pro.local>
2018-05-01 21:40:40 +0300

    add tradingview widget

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare const TradingView: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  ngAfterViewInit() {
+    new TradingView.widget({
+       'container_id': 'technical-analysis',
+       'autosize': true,
+       'symbol': 'ETHBTC',
+       'interval': '120',
+       'timezone': 'exchange',
+       'theme': 'Dark',
+       'style': '1',
+       'toolbar_bg': '#f1f3f6',
+       'withdateranges': true,
+       'hide_side_toolbar': false,
+       'allow_symbol_change': true,
+       'save_image': false,
+       'hideideas': true,
+       'studies': [ 
+       'MASimple@tv-basicstudies' ],
+       'show_popup_button': true,
+       'popup_width': '1000',
+       'popup_height': '650'
+     });
+   }
 }
